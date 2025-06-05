772664fcbd3d583fe0cfffa165120b7176ff1989
Dustin Yonak <dyonak@gmail.com>
2025-06-05 16:18:17 -0500

    Updated install.sh with super cute emojis for progress echos

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #Update the system
-echo "Setting up album player - Updating the system (1/6)"
+echo "1️⃣ Setting up album player - Updating the system (1/6)"
 sudo apt-get update && sudo apt-get -y upgrade
 
 #Install pip and setup python venv
-echo "Setting up album player - Installing pip and git (2/6)"
+echo "2️⃣ Setting up album player - Installing pip and git (2/6)"
 sudo apt-get install -y python3-pip git
 
 #enable SPI
-echo "Setting up album player - Enabling spi (3/6)"
+echo "3️⃣ Setting up album player - Enabling spi (3/6)"
 sudo dtparam spi=on
 
 #Install PifiConnector to manage wifi connection
 #Setup directory, grab from github, create venv
-echo "Setting up album player - Installing PifiConnector (4/6)"
+echo "4️⃣ Setting up album player - Installing PifiConnector (4/6)"
 cd ~
 git clone https://github.com/dyonak/PifiConnector
 cd ~/PifiConnector
@@ -21,7 +21,7 @@ source wifivenv/bin/activate
 pip install flask requests
 
 #Create and enable the service
-echo "Setting up album player - Creating and enabling PifiConnector service (5/6)"
+echo "5️⃣ Setting up album player - Creating and enabling PifiConnector service (5/6)"
 sed -i -e "s/USERNAME/$USER/g" wificonnect.service
 sudo cp wificonnect.service /etc/systemd/system
 sudo systemctl enable wificonnect.service
@@ -29,16 +29,17 @@ sudo systemctl daemon-reload
 sudo systemctl start wificonnect.service
 
 #install docker
-echo "Setting up album player - Installing docker (6/6)"
+echo "6️⃣ Setting up album player - Installing docker (6/6)"
 mkdir ~/docker && cd ~/docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 chmod +x get-docker.sh
 ./get-docker.sh
 sudo usermod -aG docker $USER
 
-echo "Album player setup complete. In order for the user group changes to take effect you need to exit and reconnect to a new session."
-echo "After reconnecting you can run the following command to start the album player:"
-echo "docker run -d --restart always --privileged --net=host dyonak/albumplayer:latest"
+echo "✅ Album player setup complete"
+echo "🛑 In order for the user group changes to take effect you need to exit and reconnect to a new session."
+echo "ℹ️ After reconnecting you can run the following command to start the album player:"
+echo "⚙️ docker run -d --restart always --privileged --net=host dyonak/albumplayer:latest"
 ##### WILL NEED TO exit SSH session and reconnect for this to take effect
 #After reconnecting run
 #docker run -d --restart always --privileged --net=host dyonak/albumplayer:latest
