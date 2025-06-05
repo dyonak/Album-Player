b2faca74ce71eca6b6c2be2d4be40c5401590cb7
dyonak <dyonak@gmail.com>
2025-06-05 15:06:12 -0500

    Updates for install.sh to automate full docker setup minus docker run command

--- install.sh
+++ install.sh
@@ -2,6 +2,32 @@
 sudo apt-get update && sudo apt-get -y upgrade
 
 #Install pip and setup python venv
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip git
 python3 -m pip install --upgrade pip
 
+#enable SPI
+sudo dtparam spi=on
+
+#Install PifiConnector to manage wifi connection
+#Setup directory, grab from github, create venv
+cd ~
+git clone https://github.com/dyonak/PifiConnector
+cd ~/PifiConnector
+python -m venv wifivenv
+pip install flask requests
+
+#Create and enable the service
+sed -i -e "s/USERNAME/$USER/g" wificonnect.service
+sudo cp wificonnect.service /etc/systemd/system
+sudo systemctl enable wificonnect.service
+sudo systemctl daemon-reload
+sudo systemctl start wificonnect.service
+
+#install docker
+mkdir ~/docker && cd ~/docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
+sudo usermod -aG docker %{USER}
+##### WILL NEED TO exit SSH session and reconnect for this to take effect
+#After reconnecting run
+#docker run -d --restart always --privileged --net=host dyonak/albumplayer:latest
